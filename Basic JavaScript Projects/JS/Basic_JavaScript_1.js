5adb26b4b4d9bd07886010520a3c42d918f5a824
Jesse G <56103989+graffje@users.noreply.github.com>
2020-08-15 04:43:45 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,16 @@
+window.alert("Hello, World!"); // displays alert window with "Hello, World" text
+
+var x = "\"I love the Blazers!\""; // assign str variable with quotation marks to "x"
+var y = "Jesse said to himself."; // assign str variable to "y"
+document.write(x + " " + y); // concatenate and display x, [space], y
+document.write("<br>") // line break
+
+var PG = "Dame", SG = "CJ", SF = "Gary", PF = "Melo", C = "Nurk"; //Assigning variables
+var PG = PG.fontcolor("green"); // making PG variable green
+var SG = SG.fontcolor("red"); // making SG variable red
+var SF = SF.fontcolor("blue"); // making SF variable blue
+var PF = PF.fontcolor("gray"); // making PF variable gray
+var C = C.fontcolor("pink"); // making C variable pink
+document.write(PG, " ", PF); // calling for screen to display PG and PF variables
+document.write("<br>") // line break
+document.write(15+3); // displaying the result of the expression 15+3
