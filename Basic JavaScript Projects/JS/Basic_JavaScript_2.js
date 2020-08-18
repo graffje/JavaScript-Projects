47dee69eb607f831de65668bfdf6ccc893441be1
Jesse G <56103989+graffje@users.noreply.github.com>
2020-08-18 09:58:34 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function newFunction() {
+    var sentence = "I hope that this";
+    sentence += " works a lot better than the last one.";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
